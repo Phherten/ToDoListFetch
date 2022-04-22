1c8a0401b531fca8e02a17a8152436c956e87303
Phherten <phigueraherten@gmail.com>
2022-04-22 19:57:39 +0000

    diseño acabado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
-	return <div></div>;
+	const [tarea, addTarea] = useState([]);
+	const [nuevaTarea, addNuevaTarea] = useState("");
+
+	return (
+		<div className="container d-flex flex-column align-items-center">
+			<div className="row">
+				<div className="col-12 mt-5">
+					<h1>tareas</h1>
+				</div>
+			</div>
+			<div className="row d-flex flex-column align-items-center mt-5">
+				<div className="tareas" id="tareas">
+					<input
+						type="text"
+						class="form-control-lg border-0"
+						placeholder="introduce tarea"
+						ide="tarea"></input>
+				</div>
+				<div className="hojaDos" id="tareasdos"></div>
+				<div className="hojaTres" id="tareastres"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
