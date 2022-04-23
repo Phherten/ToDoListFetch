caa12fe522f4a41aa561b6d1ab2fe8ae11616e7a
Phherten <phigueraherten@gmail.com>
2022-04-23 17:42:10 +0000

    falta borrar los elementos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-	const [tarea, addTarea] = useState([]);
-	const [nuevaTarea, addNuevaTarea] = useState("");
+	const [tarea, addTarea] = useState("");
+	const [tareas, addNuevaTarea] = useState([]);
+	useEffect(() => {
+		console.log(tareas);
+	}, [tareas]);
+	const arrayTareas = tareas.map((tareaIndividual, i) => {
+		return (
+			<div
+				className="d-flex justify-content-between pb-2 ps-3"
+				id="lineaTarea">
+				{tareaIndividual}
 
+				<button className="borrar" id="papelera">
+					<i class="fas fa-trash"></i>
+				</button>
+			</div>
+		);
+	});
 	return (
 		<div className="container d-flex flex-column align-items-center">
 			<div className="row">
@@ -13,12 +28,24 @@ const Home = () => {
 				</div>
 			</div>
 			<div className="row d-flex flex-column align-items-center mt-5">
-				<div className="tareas" id="tareas">
-					<input
-						type="text"
-						class="form-control-lg border-0"
-						placeholder="introduce tarea"
-						ide="tarea"></input>
+				<div className="tareas pb-5" id="tareas">
+					<div className="lineaInput d-flex justify-content-between">
+						<input
+							type="text"
+							class="form-control-lg border-0"
+							placeholder="introduce tarea"
+							id="tarea"
+							onChange={(e) => addTarea(e.target.value)}
+							value={tarea}
+							autoComplete="off"
+						/>
+						<button
+							className="agregarTarea"
+							onClick={() => addNuevaTarea([...tareas, tarea])}>
+							<i class="fas fa-plus-circle"></i>
+						</button>
+					</div>
+					<div className="texto">{arrayTareas}</div>
 				</div>
 				<div className="hojaDos" id="tareasdos"></div>
 				<div className="hojaTres" id="tareastres"></div>
